59d147357d46fc41c33e8523eb6e4e10f89649bc
Ganesh Borole <ganeshborole73@gmail.com>
2025-02-22 13:02:12 +0530

    addinf a parent folder to main branch

---
+++ 02Counter/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+
+function App() {
+  return (
+    <>
+      <h1 className="text-black bg-green-500 p-4 rounded">Tailwind is working</h1>
+      <div className="bg-blue-500 text-white p-4">This has a blue background.</div>
+    </>
+  );
+}
+
+export default App;
